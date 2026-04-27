17ad09a4a17b9a6f79e106c2771018168455d708
Joshua Revels <139186811+jmrevels@users.noreply.github.com>
2026-04-27 13:30:09 -0400

    Added Outline for main procedure

--- mole.asm
+++ mole.asm
@@ -29,6 +29,19 @@ main PROC
 	INVOKE MessageBox, hMainWnd, ADDR testText,
 		ADDR testTitle, MB_OK
 
+	# Start of gameplay loop
+	#	(Loop can be based on either a systemtime time limit or # of moles)
+	# Get random coordinates for window
+	# Get system time at start of window
+	# Display window
+	# On window close, get system time again
+	#	Score is proportional to difference in system time
+	# Add score to total score
+	# Repeat loop if not out of time/moles
+	# Display score
+	# Display high score/save score to file if higher
+
+
 main ENDP
 
 END main
