e68c604d7cec3c6996ad733afba563897c2bdfce
Joshua Revels <139186811+jmrevels@users.noreply.github.com>
2026-04-27 07:14:26 -0400

    Added data variable/structure

--- mole.asm
+++ mole.asm
@@ -1,6 +1,18 @@
 INCLUDE Irvine32.inc
 
 .data
+score WORD 0
+
+SYSTEMTIME STRUCT
+	wYear			WORD ?
+	wMonth			WORD ?
+	wDayOfWeek		WORD ?
+	wDay			WORD ?
+	wHour			WORD ?
+	wMinute			WORD ?
+	wSecond			WORD ?
+	wMilliseconds	WORD ?
+SYSTEMTIME ENDS
 
 .code
 
