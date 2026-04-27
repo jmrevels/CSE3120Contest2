5cd9249d548a5dce0a90d474bafa27c98be32d5a
Joshua Revels <139186811+jmrevels@users.noreply.github.com>
2026-04-27 18:29:45 -0400

    Added "random" function tests

--- mole.asm
+++ mole.asm
@@ -2,6 +2,11 @@ INCLUDE Irvine32.inc
 INCLUDE GraphWin.inc
 
 .data
+maxXcoord DWORD 1920d
+maxYcoord DWORD 1080d
+xCoord DWORD ?
+yCoord DWORD ?
+
 score WORD 0
 
 testText BYTE "test",0
@@ -26,20 +31,30 @@ SYSTEMTIME ENDS
 .code
 
 main PROC
+	call Randomize
 	INVOKE MessageBox, hMainWnd, ADDR testText,
 		ADDR testTitle, MB_OK
 
-	# Start of gameplay loop
-	#	(Loop can be based on either a systemtime time limit or # of moles)
-	# Get random coordinates for window
-	# Get system time at start of window
-	# Display window
-	# On window close, get system time again
-	#	Score is proportional to difference in system time
-	# Add score to total score
-	# Repeat loop if not out of time/moles
-	# Display score
-	# Display high score/save score to file if higher
+	; Start of gameplay loop
+	;	(Loop can be based on either a systemtime time limit or # of moles)
+	Game:
+	; Get random coordinates for window
+	mov eax, maxXcoord
+	call RandomRange
+	mov xCoord, eax
+
+	mov eax, maxYcoord
+	call RandomRange
+	mov yCoord, eax
+
+	; Get system time at start of window
+	; Display window
+	; On window close, get system time again
+	;	Score is proportional to difference in system time
+	; Add score to total score
+	; Repeat loop if not out of time/moles
+	; Display score
+	; Display high score/save score to file if higher
 
 
 main ENDP
